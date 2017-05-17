ea44298231b5166af1178059cbd5b0dd846482e7
Raine Padilla <epadilla7@mail.greenriver.edu>
2017-05-17 16:32:51 -0500

    Step 2 complete

--- intro.js
+++ intro.js
@@ -6,3 +6,32 @@
 
 // Add a statement to your file that prints "Introduction to JavaScript" to the console window
 console.log("Introduction to JavaScript");
+
+// Function test area
+FizzBuzz();
+
+
+// Write a function that prints 'fizz' if a number is divisible by three,
+// 'buzz' if it is divisible by 5, or 'fizzbuzz' if it's both.
+function FizzBuzz()
+{
+	for(var i = 1; i <= 100; i++)
+	{
+		if(i % 3 === 0 && i % 5 === 0)
+		{
+			console.log("FizzBuzz");
+		}
+		else if(i % 3 === 0)
+		{
+			console.log("Fizz");
+		}
+		else if(i % 5 === 0)
+		{
+			console.log("Buzz");
+		}
+		else
+		{
+			console.log(i);
+		}
+	}
+}
