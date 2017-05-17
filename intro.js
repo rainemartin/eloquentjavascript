7a54d06d659fd6eadd47fb5fa5e331686114aa35
Raine Padilla <epadilla7@mail.greenriver.edu>
2017-05-17 18:02:35 -0500

    Step 4 complete

--- intro.js
+++ intro.js
@@ -53,3 +53,16 @@ function CountBs(string)
 	return count;
 }
 
+// Write a function to count a given character in a given string
+function CountChar(string, character)
+{
+	var count = 0;
+	for(var i = 0; i < string.length; i++)
+	{
+		if(string.charAt(i) == character)
+		{
+			count++;
+		}
+	}
+	return count;
+}
