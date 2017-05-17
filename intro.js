7f7c26f0ef1e39e2d289b3a17f602da1a8f5d337
Raine Padilla <epadilla7@mail.greenriver.edu>
2017-05-17 16:17:02 -0500

    Step 1 complete

---
+++ intro.js
@@ -0,0 +1,8 @@
+/*
+ * Raine Padilla
+ * Intro to JS Exercise
+ * 5/17/2017
+ */
+
+// Add a statement to your file that prints "Introduction to JavaScript" to the console window
+console.log("Introduction to JavaScript");
